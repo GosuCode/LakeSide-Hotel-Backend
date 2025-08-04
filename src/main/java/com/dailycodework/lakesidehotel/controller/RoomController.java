909351d3b6723bddc2b2e6f45771621838d08054
GosuCode <gosucode1945@gmail.com>
2025-08-04 20:08:07 +0545

    refactor: optimize room response creation and update token expiration to 24 hours

--- src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
@@ -91,13 +91,13 @@ public class RoomController {
         List<Room> rooms = roomService.getAllRooms();
         List<RoomResponse> roomResponses = new ArrayList<>();
         for (Room room : rooms) {
+            RoomResponse roomResponse = getRoomResponse(room);
             byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
             if (photoBytes != null && photoBytes.length > 0) {
                 String base64Photo = Base64.encodeBase64String(photoBytes);
-                RoomResponse roomResponse = getRoomResponse(room);
                 roomResponse.setPhoto(base64Photo);
-                roomResponses.add(roomResponse);
             }
+            roomResponses.add(roomResponse);
         }
         return ResponseEntity.ok(roomResponses);
     }
